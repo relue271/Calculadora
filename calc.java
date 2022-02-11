0edb4b5a59cf750ec79b337d2236e4050dc9f0c6
Euler G Gonçalves <goncalves.euler@gmail.com>
2022-02-10 21:50:09 -0300

    Update calc.java
    versao 3

--- calc.java
+++ calc.java
@@ -1,20 +1,51 @@
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
 
-public class exemplo{
-    public static void main(String[] args){
+
+public class calccien { // Objeto Janela
+    public static void main (String args[]) {
+
+
         
-        Scanner integerValueA = new Scanner(System.in);
-        int intValA = integerValueA.nextInt();
-        
+        /*
+        Panel c =new Panel();
+        c .setLayout(new GridLayout(2,2));
+        c.add(new Button("um"));
+        c.add(new TextField(5));
+        c.add(new Button("dois"));
+        c.add(new Button("três"));
+        */
 
-        Scanner integerValueB = new Scanner(System.in);
-        int intValB = integerValueB.nextInt();
-        
 
-        int PROD = intValA * intValB;
+        /*
+        JFrame frame = new JFrame("Swing Application");
+        JButton but = new JButton("I am a Swing button");
+        JLabel texto = new JLabel("Number of button clicks: 0");
+        JPanel painel = new JPanel( );
+        painel.add(but);
+        painel.add(texto);
+        frame.getContentPane( ).add(painel);
+        frame.pack( );
+        frame.show( );
+        */
 
-        System.out.println("PROD = " + PROD);
 
-        return;
+        /*
+        int i;
+        JFrame janela = new JFrame("calccien do relue271");
+        janela.setBounds(50, 100, 400, 150); // Seta posição e tamanho
+        janela.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        FlowLayout flow = new FlowLayout(FlowLayout.LEFT); // Define o layout do container
+        Container caixa = janela.getContentPane(); // Define o tamanho
+        caixa.setLayout(flow); // Seta layout do container
+        for (i=1; i<=6; i++)
+        caixa.add(new JButton("Aperte " + i)); // Adiciona um botão
+        janela.setVisible(true); // Exibe a janela
+        JLabel label = new JLabel("Exemplo de texto:");
+        caixa.add(label);
+        JTextField campo = new JTextField(15);
+        caixa.add(campo);
+        janela.pack(); // Redimensiona a janela
+        */
     }
 }
